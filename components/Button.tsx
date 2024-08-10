c933d252ba16faba86c2245c1f6e1774073f54f5
Seb <scastillo.hgo@gmail.com>
2024-08-09 23:30:51 -0400

    server and button component

---
+++ components/Button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+
+interface CustomButtonProps {
+  title: string;
+  color: string;
+  onPress: () => void; // Function to handle button press
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({ title, color, onPress }) => {
+  return (
+    <View style={styles.container}>
+      <Button 
+        title={title}
+        color={color}
+        onPress={onPress}
+      />
+    </View>
+
+
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+
+
+
+});
+
+export default CustomButton;
