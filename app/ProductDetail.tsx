a3cb171a824bcc3cc0e56696badfd3d5a3435be2
Seb <scastillo.hgo@gmail.com>
2024-08-20 01:33:08 -0400

    add productdetail.tsx

---
+++ app/ProductDetail.tsx
@@ -0,0 +1,32 @@
+// // import { View, Text } from "react-native";
+// // import React from "react";
+
+// // const ProductDetail = () => {
+// //   const renderItem = ({ item }) => (
+// //     <View className="p-4 mb-4 bg-gray-800 rounded-lg shadow-md">
+// //       <View className="flex-row justify-between items-center mb-2">
+// //         <Text className="text-lg font-semibold text-white">Quantity</Text>
+// //         <Text className="text-lg font-medium text-gray-300">
+// //           {item.quantity}
+// //         </Text>
+// //       </View>
+// //       <View className="flex-row justify-between items-center">
+// //         <Text className="text-lg font-semibold text-white">Date</Text>
+// //         <Text className="text-lg font-medium text-gray-300">
+// //           {new Date(item.date).toLocaleDateString()}
+// //         </Text>
+// //       </View>
+// //     </View>
+// //   );
+// //   return (
+// //     <SafeAreaView>
+// //       <FlatList
+// //         data={products}
+// //         renderItem={renderItem}
+// //         keyExtractor={(item) => item.id.toString()} // Asegúrate de que `item.id` sea único
+// //       />
+// //     </SafeAreaView>
+// //   );
+// // };
+
+// export default ProductDetail;
