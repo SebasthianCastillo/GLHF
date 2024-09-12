700dbd8a40ab0429bd0515114cfd4ce2c7b96d6b
Seb <scastillo.hgo@gmail.com>
2024-09-11 23:59:13 -0300

    Enviroment variables DB_URL

--- api/server.mjs
+++ api/server.mjs
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -6,26 +8,23 @@ import Category from "./model/Category.js";
 import Producto from "./model/Producto.js";
 import ProductDetail from "./model/ProductDetail.js";
 import { MongoClient, ObjectId } from "mongodb";
-// const Category = require("./model/Category.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 // Connect to MongoDB
-const MONGO_URI = process.env.DB_URL;
+const DB_URL = process.env.DB_URL;
+
 mongoose
-  .connect(MONGO_URI, {})
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("Error connecting to MongoDB", error);
-  });
+  .connect(DB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.post("/addCategory", async (req, res) => {
